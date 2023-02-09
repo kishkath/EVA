5f70a8c4f54a223621100717956715ea8153d748
Kishkath <60026221+kishkath@users.noreply.github.com>
2023-02-09 13:18:48 +0530

    filled receptive Field

--- 6 - Advanced Convolutions/model.py
+++ 6 - Advanced Convolutions/model.py
@@ -20,57 +20,114 @@ class Net(nn.Module):
             nn.BatchNorm2d(16),
             nn.ReLU(),
             # (32+2-3) + 1 = 32
-           
+            # Jump_in = 1, Jump_out = Jump_in*s
+            # RF = 1
+            # RF_out = Rin + (K-1) * Jin
+            
+            ## Jump_out = 1*1
+            # RF_in = 1*1 = 1
+            # RF_out = 1 + (3-1)*1 = 1 + (2*1) = 3 
+            
+            # RF_in = 1, RF_out = 3, Jin = 1
             nn.Conv2d(in_channels=16,out_channels=32,kernel_size=(3,3)),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             # (32-3) + 1 = 30
+            
+            # Jump_out = Jump_in*s = 1*1 = 1
+            # RF_in = 3 
+            # RF_out = RF_in + (K-1) * Jin = 3 + (3-1)*1 = 3 + 2 = 5 
+            
+            # RF_in = 3, RF_out = 5, Jin = 1
             nn.Conv2d(in_channels=32,out_channels=32,kernel_size=(3,3)),
             nn.BatchNorm2d(32),
             nn.ReLU())
             # (30-3)+ 1 = 28
             
-        
+            # RF_in = 5 
+            # Jump_out = Jin*s = 1 
+            # RF_out = RF_in + (K-1)*Jin = 5 + (3-1)*1 = 5 + 2 = 7
+            
+        # RF_in = 5, RF_out = 7, Jin = 1
         self.conv2 = nn.Sequential(
             nn.Conv2d(in_channels=32,out_channels=128,kernel_size=(3,3),dilation=2),
             nn.BatchNorm2d(128),
             nn.ReLU(),
             # (28-5)/1+ 1 = 24
+            
+            # RF_in = 7 
+            # Jump_out = Jin*s = 1 
+            # RF_out = RF_in + (K-1)*Jin = 7 + (5-1)*1 = 7 + 4 = 11 
            
-          
+            # RF_in = 7, RF_out = 11, Jin = 1
             nn.Conv2d(in_channels=128,out_channels=64,kernel_size=(3,3)),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             # (24-2) + 1 = 22
-           
+            
+            # RF_in = 11 
+            # Jump_out = Jin*s = 1
+            # RF_out = RF_in + (K-1)*Jin = 11 + (3-1)*1 = 11 + 2 = 13 
+            
+            # RF_in = 11,RF_out=13,Jin=1
             nn.Conv2d(in_channels=64,out_channels=32,kernel_size=(3,3),padding=1,stride=2),
             nn.BatchNorm2d(32),
             nn.ReLU())
             # (22+2-3)/2+1 = 11
             
+            # RF_in = 13 
+            # Jump_out = Jin*s = 1*2 = 2
+            # RF_out = RF_in + (K-1)*Jin = 13 + (3-1)*1 = 13 + 2 = 15
+            
+            # RF_in = 13, RF_out = 15, Jin = Jump_out = 2
         self.conv3 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=128, kernel_size=3, groups=8),
             nn.BatchNorm2d(128),
             # 9
+            
+            # RF_in = 15 
+            # Jump_out = Jin*s = 2*1 = 2 
+            # RF_out = RF_in + (K-1)*Jin = 15 + (3-1)*2 = 15 + 4 = 19
             nn.Conv2d(in_channels=128, out_channels=64, kernel_size=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
+            
             # 9
+            
+            # RF_in = 19 
+            # Jump_out = 2 
+            # RF_out = RF_in+(K-1)*Jin = 19 + (1-1)*2 = 19
             nn.Conv2d(in_channels=64,out_channels=32,kernel_size=(3,3)),
             nn.BatchNorm2d(32),
             nn.ReLU())
             # (9-3)/1 + 1 = 7
+            
+            # RF_in = 19
+            # Jump_out = 2
+            # RF_out = RF_in + (K-1)*Jin = 19 + (3-1)* 2 = 19 + 4 = 23
             
         self.conv4 = nn.Sequential(
             nn.Conv2d(in_channels=32,out_channels=32,kernel_size=(3,3)),
             # (7-3) + 1 = 5
             nn.BatchNorm2d(32),
             nn.ReLU(),
+            
+            # RF_in = 23 
+            # Jump_out = 2 
+            # RF_out = RF_in + (K-1)*Jn = 23 + (3-1)*2 = 23 + 4 = 25
+            
             nn.Conv2d(in_channels=32,out_channels=16,kernel_size=(1,1)),
             nn.BatchNorm2d(16),
             # (5-1) + 1 = 5
+            
+            # RF_in = 25 
+            # Jump_out = 2 
+            # RF_out = RF_in + (K-1)*Jin = 25 + (1-1)*2 = 25
             nn.Conv2d(in_channels=16,out_channels=10,kernel_size=(3,3)))
-
+           
+            # RF_in = 25 
+            # Jump_out = 2
+            # RF_out = RF_in + (K-1)*Jin = 25 + (3-1)*2 = 29
         self.gap = nn.AvgPool2d(3)
         
         self.dropout = nn.Dropout2d(0.1)
